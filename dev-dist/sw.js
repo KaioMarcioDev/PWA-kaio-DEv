8d1bc5dc6722b1cbb1d47ba43fa68beeffb8ca43
Kaio Marcio Dev <89593379+KaioMarcioDev@users.noreply.github.com>
2025-09-07 18:11:14 -0300

    PWA Kaio

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.164uotf024g"
+    "revision": "0.ks16jc0a5p8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
